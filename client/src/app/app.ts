645e3881e0e81b29809a9a4eafc69eccd0ca3cc4
marshel c mathews <marshelmathews65@gmail.com>
2025-07-21 20:32:12 +0530

    section2 final

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,13 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit{
+  protected readonly title = 'client';
+  private http = inject(HttpClient);
+
+    ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/User").subscribe({
+      next : response => console.log(response),
+      error : error => console.error(error),
+      complete : () => console.log("completed http request")
+      
+    }
+      
+    )
+  }
+
 }
